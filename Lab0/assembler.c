6f7a88914de6bbb848b9590c47a67d67fc7079de
zismaelm <ismaelmarquez@utexas.edu>
2018-09-19 10:41:20 -0500

    Created Lab0 directory and assembler.c file. Began adding starter parser functions to assembler.

---
+++ Lab0/assembler.c
@@ -0,0 +1,111 @@
+#include <stdio.h> /* standard input/output library */
+#include <stdlib.h> /* Standard C Library */
+#include <string.h> /* String operations library */
+#include <ctype.h> /* Library for useful character operations */
+#include <limits.h> /* Library for definitions of common variable type characteristics */
+
+FILE *infile = NULL;
+FILE *outfile = NULL;
+
+int main(int argc, char* argv[]) {
+	printf("---START---\n");
+
+	// parse the command line arguments
+	char *prgName = NULL;
+	char *iFileName = NULL;
+	char *oFileName = NULL;
+
+	prgName = argc[0];
+	iFileName = argv[1];
+	oFileName = argv[2];
+
+	printf("program name = '%s'\n", prgName);
+	printf("input file name = '%s'\n", iFileName);
+	printf("output file name = '%s'\n", oFileName);
+
+	// open the input and output files
+	infile = fopen(argv[1], "r");
+	outfile = fopen(argv[2], "w");
+
+	if (!infile) {
+		printf("Error: Cannot open file %s\n", argv[1]);
+		exit(4);
+	}
+	if (!outfile) {
+		printf("Error: Cannot open file %s\n", argv[2]);
+		exit(4);
+	}
+
+	/* Translate the input file and write to the output file */
+
+	fclose(infile);
+	fclose(outfile);
+
+	return 0;
+}
+
+/* Converts a string into a hex or decimal number */
+int toNum( char * pStr ) {
+   char * t_ptr;
+   char * orig_pStr;
+   int t_length,k;
+   int lNum, lNeg = 0;
+   long int lNumLong;
+
+   orig_pStr = pStr;
+   if( *pStr == '#' )				/* decimal */
+   { 
+     pStr++;
+     if( *pStr == '-' )				/* dec is negative */
+     {
+       lNeg = 1;
+       pStr++;
+     }
+     t_ptr = pStr;
+     t_length = strlen(t_ptr);
+     for(k=0;k < t_length;k++)
+     {
+       if (!isdigit(*t_ptr))
+       {
+	 printf("Error: invalid decimal operand, %s\n",orig_pStr);
+	 exit(4);
+       }
+       t_ptr++;
+     }
+     lNum = atoi(pStr);
+     if (lNeg)
+       lNum = -lNum;
+ 
+     return lNum;
+   }
+   else if( *pStr == 'x' )	/* hex     */
+   {
+     pStr++;
+     if( *pStr == '-' )				/* hex is negative */
+     {
+       lNeg = 1;
+       pStr++;
+     }
+     t_ptr = pStr;
+     t_length = strlen(t_ptr);
+     for(k=0;k < t_length;k++)
+     {
+       if (!isxdigit(*t_ptr))
+       {
+	 printf("Error: invalid hex operand, %s\n",orig_pStr);
+	 exit(4);
+       }
+       t_ptr++;
+     }
+     lNumLong = strtol(pStr, NULL, 16);    /* convert hex string into integer */
+     lNum = (lNumLong > INT_MAX)? INT_MAX : lNumLong;
+     if( lNeg )
+       lNum = -lNum;
+     return lNum;
+   }
+   else
+   {
+	printf( "Error: invalid operand, %s\n", orig_pStr);
+	exit(4);  /* This has been changed from error code 3 to error code 4, see clarification 12 */
+   }
+}
